20998cae3987a518d6aa5d26d7feb46ab2d59f54
Mauricio Rojas <mrojasb2000@gmail.com>
2020-08-12 10:59:50 -0400

    Refactor retrieving the Golang version.

--- chapter1/recipe01/main.go
+++ chapter1/recipe01/main.go
@@ -6,8 +6,8 @@ import (
 )
 
 const info = `
-  Application %s starting.
-  The binary was build by GO: %s`
+Application %s starting.
+The binary was build by GO: %s`
 
 func main() {
 	log.Printf(info, "Example", runtime.Version())
