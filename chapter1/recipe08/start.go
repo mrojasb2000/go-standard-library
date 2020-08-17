34426ddf26b6b66e6e04e33d46ed014620f56920
Mauricio Rojas <mrojasb2000@gmail.com>
2020-08-17 17:37:54 -0400

    Add comments

--- chapter1/recipe08/start.go
+++ chapter1/recipe08/start.go
@@ -6,6 +6,11 @@ import (
 	"os/exec"
 )
 
+// The process could be executed asynchronously too.
+// This is done by calling the start methods of the Cmd structure. In this case, the
+// process is executed, but the main gorutine does not wait until it ends.
+// The Wait method could be used to wait until the process ends. After the Wait method finishes,
+// the resources of the process are released.
 func main() {
 	prc := exec.Command("ls", "-a")
 	out := bytes.NewBuffer([]byte{})
